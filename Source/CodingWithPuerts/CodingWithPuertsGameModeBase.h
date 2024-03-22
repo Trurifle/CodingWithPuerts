adebe55b21e2670716c804d04b01036f055fbd3e
wlxstmm <3215788331@qq.com>
2024-03-22 20:49:24 +0800

    Init Commit

---
+++ Source/CodingWithPuerts/CodingWithPuertsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodingWithPuertsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODINGWITHPUERTS_API ACodingWithPuertsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
